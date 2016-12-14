76889a5fc9516af6b7bc6add01f3d8227292fdb6
LordZEDith <LordZEDith@users.noreply.github.com>
2016-12-14 09:26:55 -0700

    Fixed Draven and Ezreal DMG

---
+++ BaseUlt3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace BaseUlt3
+{
+    class Program
+    {
+        public static BaseUlt BaseUlt;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            BaseUlt = new BaseUlt();
+            Game.PrintChat("BaseUlt Updated by LordZEDith");
+            Game.PrintChat("Draven and Ezreal Damage Updated");
+        }
+    }
+}
