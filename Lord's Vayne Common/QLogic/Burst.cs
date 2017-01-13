0f0a677f8b565d5e1fca38e5d20af9b2569e9e42
LordZEDith <LordZEDith@users.noreply.github.com>
2017-01-13 09:48:53 -0700

    Uses Common Orbwalker

---
+++ Lord's Vayne Common/QLogic/Burst.cs
@@ -0,0 +1,37 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lord_s_Vayne.QLogic
+{
+    class Bursts
+    {
+        public static void Burst()
+        {
+           
+            var target = TargetSelector.GetTarget(Program.E.Range, TargetSelector.DamageType.Physical);
+            if (!target.IsValidTarget())
+            {
+                return;
+            }
+           else
+                if (Program.Q.IsReady() && target.IsValidTarget(600) && !Program.qmenu.Item("FastQ").GetValue<bool>() &&
+                    target.GetBuffCount("vaynesilvereddebuff") == 2)
+                {
+                    Program.Q.Cast(Game.CursorPos);
+
+                }
+                else if (Program.Q.IsReady() && target.IsValidTarget(600) && Program.qmenu.Item("FastQ").GetValue<bool>() &&
+                    target.GetBuffCount("vaynesilvereddebuff") == 2 )
+                {
+                    Program.Q.Cast(Game.CursorPos);
+                    Game.SendEmote(Emote.Dance);
+                }
+            }
+        
+    }
+}
